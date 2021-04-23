db2dc393870ce102c0615d07932a0a0a50eca947
nicholasc <nicholas.charbonneau@gmail.com>
2021-04-23 00:42:11 -0400

    glad output folder

--- cmake/glad.cmake
+++ cmake/glad.cmake
@@ -6,3 +6,10 @@ set(GLAD_SOURCES libs/glad/src/glad.c)
 
 # add library target
 add_library(glad ${GLAD_SOURCES})
+
+# set target properties
+set_target_properties(glad
+  PROPERTIES
+   LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/libs/glad
+   ARCHIVE_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/libs/glad
+)
