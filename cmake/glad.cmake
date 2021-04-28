aabbc0cbcd0a9444661728a01c4e3de56b62f5df
nicholasc <nicholas.charbonneau@gmail.com>
2021-04-28 09:25:24 -0400

    update to glad2

--- cmake/glad.cmake
+++ cmake/glad.cmake
@@ -2,7 +2,7 @@
 include_directories(libs/glad/include)
 
 # define source files
-set(GLAD_SOURCES libs/glad/src/glad.c)
+set(GLAD_SOURCES libs/glad/src/gl.c)
 
 # add library target
 add_library(glad ${GLAD_SOURCES})
