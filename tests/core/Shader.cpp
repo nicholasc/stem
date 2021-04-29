6797529cd12154d2c5c3a650b09d65e538a90055
nicholasc <nicholas.charbonneau@gmail.com>
2021-04-28 20:59:00 -0400

    valid shader code test

--- tests/core/Shader.cpp
+++ tests/core/Shader.cpp
@@ -17,6 +17,11 @@ TEST_CASE("stem::Shader class", "[core]") {
     REQUIRE_THROWS_AS(vertex.compile(), stem::ShaderSyntaxError);
   }
 
+  SECTION("valid shader does compile") {
+    vertex.setSource("void main() { }");
+    vertex.compile();
+  }
+
   SECTION("id is zero after destroy") {
     vertex.destroy();
     REQUIRE(vertex.getId() == 0);
